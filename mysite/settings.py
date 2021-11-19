ddd7ee88f579a14b3ed9756045dd4bd196e60449
shvetsantonyx <90096233+shvetsantonyx@users.noreply.github.com>
2021-11-19 21:28:04 +0300

    Created HTML-templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rh=@_w(w#78v(g&-ld28yngnbs3@#mbqd=@n$_cc_gyjc@rq)n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
