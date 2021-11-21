5d6dfb20ef04cfd6e6d8ca17040c71f89f14f925
shvetsantonyx <90096233+shvetsantonyx@users.noreply.github.com>
2021-11-21 23:33:08 +0300

    Some changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,7 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
