3db23abf7ec1d9c88ec054e9aa537c0fdfb9221d
Shvets Anton <wakeup999@yandex.ru>
2021-11-29 13:32:46 +0300

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rh=@_w(w#78v(g&-ld28yngnbs3@#mbqd=@n$_cc_gyjc@rq)n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
